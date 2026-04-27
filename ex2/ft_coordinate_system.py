808885e87a9d7b6ebd8f47b7fd8e47d22b024a08
edsole-a <edsole-a@student.42madrid.com>
2026-04-27 14:30:23 +0200

    RE-refacatored to make it even more understandable, added base (0,0,0) case

--- ex2/ft_coordinate_system.py
+++ ex2/ft_coordinate_system.py
@@ -1,11 +1,12 @@
 import math
 
 
-def parse_coords(raw: str) -> tuple[float, ...] | None:
+def get_player_pos() -> tuple[float, ...]:
+    raw = input("Enter new coordinates as floats in format 'x,y,z': ")
     args = raw.split(",")
     if len(args) != 3:
         print("Invalid syntax")
-        return None
+        return get_player_pos()
     result: tuple[float, ...] = ()
     i = 0
     while i < len(args):
@@ -13,20 +14,15 @@ def parse_coords(raw: str) -> tuple[float, ...] | None:
             result = result + (float(args[i]),)
         except ValueError as e:
             print(f"Error on parameter '{args[i].strip()}': {e}")
-            return None
+            return get_player_pos()
         i += 1
     return result
 
 
-def get_player_pos() -> tuple[float, ...]:
-    coords = None
-    while coords is None:
-        coords = parse_coords(input("Enter new coordinates as"
-                                    " floats in format 'x,y,z': "))
-    return coords
-
-
-def get_distance(pos1: tuple[float, ...], pos2: tuple[float, ...]) -> float:
+def get_distance(
+    pos1: tuple[float, ...],
+    pos2: tuple[float, ...] = (0.0, 0.0, 0.0)
+) -> float:
     return round(math.sqrt((pos2[0] - pos1[0]) ** 2
                            + (pos2[1] - pos1[1]) ** 2
                            + (pos2[2] - pos1[2]) ** 2), 4)
@@ -37,8 +33,8 @@ if __name__ == "__main__":
     print("Get a first set of coordinates")
     coords_1 = get_player_pos()
     print(f"Got a first tuple: {coords_1}")
-    print(f"It  includes: X={coords_1[0]}, Y={coords_1[1]}, Z={coords_1[2]}")
-    print(f"Distance to center: {get_distance(coords_1, (0.0, 0.0, 0.0))}\n")
+    print(f"It includes: X={coords_1[0]}, Y={coords_1[1]}, Z={coords_1[2]}")
+    print(f"Distance to center: {get_distance(coords_1)}\n")
     print("Get a second set of coordinates")
     coords_2 = get_player_pos()
     print("Distance between the 2 sets of coordinates: "
